a8c4addfab84581e02b697c0ae3819b250fabd7b
HasnaeSn <hasnae.sdn@outlook.com>
2023-08-17 16:54:10 +0200

    front-end not yet finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy], as: :delete_bookmark
+
+  # Read all
+  # get 'lists/:id/bookmarks/new', to:'lists#index'
+
+  # # Create new
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+
+  # # Read one
+  # get 'lists/:id', to:'lists#show'
 end
